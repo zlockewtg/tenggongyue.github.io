7752f98b5ee96670245304dce4d17a3fbacae43a
teng <1840409990@qq.com>
2025-07-10 19:16:14 +0800

    init

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards','projects']
+const section_names = ['home', 'publications', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
